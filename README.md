1409e000adc9bb1758f509856747d8f6738d8879
Petar Zelenovic <pzelenovic@gmail.com>
2022-11-13 08:41:09 +0100

    Initialized Spring Boot Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cqrs-booking
+
 This repository is an attempt to solve https://codingdojo.org/kata/CQRS_Booking/ kata
---
+++ src/main/java/com/mandelbro/cqrsbooking/CqrsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.mandelbro.cqrsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsBookingApplication.class, args);
+	}
+
+}
